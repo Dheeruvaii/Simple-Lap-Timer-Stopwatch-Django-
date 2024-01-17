9bf3a5495eb6d05f387d672378e331ff557dc349
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-17 12:28:34 +0545

    use datetime.timedelta func

--- Stopwatch_Project/timer_app/models.py
+++ Stopwatch_Project/timer_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+import time 
+import datetime 
 # Create your models here.
+print("after 5 sec sleep time shows")
+time.sleep(5)
+print("sleep time")
+
+
+current=datetime.datetime.now()
+print ('current time ',str(current))
+one_year=current + datetime.timedelta(days=365)
+print('one year date',str(one_year))
+
+
+seconds= time.time()
+print("time in seconds  since the epoch",str (seconds))
+local_time=time.ctime(seconds)
+print ('ctime',str(local_time))

c8c7e5bb939ec221238676c683b3ad8c4d51f416
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-17 12:51:42 +0545

    simple lap timer watch complete

--- Stopwatch_Project/timer_app/models.py
+++ Stopwatch_Project/timer_app/models.py
@@ -1,19 +1,68 @@
-from django.db import models
-import time 
-import datetime 
-# Create your models here.
-print("after 5 sec sleep time shows")
-time.sleep(5)
-print("sleep time")
 
 
-current=datetime.datetime.now()
-print ('current time ',str(current))
-one_year=current + datetime.timedelta(days=365)
-print('one year date',str(one_year))
+import time
+  
+# Timer starts
+starttime = time.time()
+lasttime = starttime
+lapnum = 1
+value = ""
+  
+print("Press ENTER for each lap.\nType Q and press ENTER to stop.")
+  
+while value.lower() != "q":
+              
+    # Input for the ENTER key press
+    value = input()
+  
+    # The current lap-time
+    laptime = round((time.time() - lasttime), 2)
+  
+    # Total time elapsed since the timer started
+    totaltime = round((time.time() - starttime), 2)
+  
+    # Printing the lap number, lap-time, and total time
+    print("Lap No. "+str(lapnum))
+    print("Total Time: "+str(totaltime))
+    print("Lap Time: "+str(laptime))
+            
+    print("*"*20)
+  
+    # Updating the previous total time and lap number
+    lasttime = time.time()
+    lapnum += 1
+  
+print("Exercise complete!")
+# from django.db import models
+
+# import time 
+# import datetime
 
 
-seconds= time.time()
-print("time in seconds  since the epoch",str (seconds))
-local_time=time.ctime(seconds)
-print ('ctime',str(local_time))
+# # timer starts
+# starttime=time.time()
+# endtime=starttime
+# lapnum= 1
+# values=""
+
+# print ("press ENTER to start new lap.\ntype Q and press ENTER to stop ")
+
+# while values.lower() != "q":
+#     value=input()
+#     laptime=round((time.time()-endtime),2)
+#     totaltime=round(time.time(),starttime)
+#     print('value',+str(value))
+#     print('laptime',+str(laptime))
+#     print('totaltime',+str(totaltime))
+             
+#     print("*"*20)
+  
+#     # Updating the previous total time and lap number
+#     lasttime = time.time()
+#     lapnum += 1
+  
+# print("Exercise complete!")
+
+
+
+

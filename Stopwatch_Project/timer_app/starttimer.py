5f623ffac0c95a1826711446c5157cc6ab6108dd
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-17 12:59:24 +0545

    complete starttimer

---
+++ Stopwatch_Project/timer_app/starttimer.py
@@ -0,0 +1,18 @@
+import time
+import datetime
+
+def countdown(h,m,s):
+    total_seconds=h*3600 + m*60 + s
+    while total_seconds > 0:
+        timer= datetime.timedelta(seconds=total_seconds)
+        print('',timer)
+        time.sleep(1)
+        total_seconds -=1
+
+        print("countdown is at zero seconds")
+
+h = input("Enter the time in hours: ")
+m = input("Enter the time in minutes: ")
+s = input("Enter the time in seconds: ")
+countdown(int(h), int(m), int(s))
+

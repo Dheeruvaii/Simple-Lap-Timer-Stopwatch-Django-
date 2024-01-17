b40d79a0f6e1952b566c9796d702991380131266
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-17 22:30:34 +0545

    add voice functionality to each sleep

--- Stopwatch_Project/timer_app/starttimer.py
+++ Stopwatch_Project/timer_app/starttimer.py
@@ -1,15 +1,24 @@
 import time
 import datetime
+import pyttsx3
 
+def speak(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)  # Speed of speech
+    engine.setProperty('volume', 0.8)  # Volume (0.0 to 1.0)
+    engine.say(text)
+    engine.runAndWait()
 def countdown(h,m,s):
     total_seconds=h*3600 + m*60 + s
 
     while total_seconds > 0:
         timer= datetime.timedelta(seconds=total_seconds)
+        speak(str(timer))
         print('',timer)
         time.sleep(5)
-        total_seconds +=2
+        total_seconds -=2
 
+        speak("countdown is at zero seconds")
         print("countdown is at zero seconds")
 
 h = input("Enter the time in hours: ")

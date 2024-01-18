77e8701a04758f29c01ec670167420b539bc9591
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-18 23:13:03 +0545

    for loop in countdown py

---
+++ Stopwatch_Project/timer_app/countownfforloop.py
@@ -0,0 +1,5 @@
+ 
+# for i in reversed(range(1, 11)):
+#     print(i)
+for i in range(10,0,-1):
+    print(i)
--- Stopwatch_Project/timer_app/elapsedtimemeasure.py
+++ Stopwatch_Project/timer_app/elapsedtimemeasure.py
@@ -1,7 +1,1 @@
 # https://www.programiz.com/python-programming/examples/elapsed-time
-from timeit import default_timer as timer
-
-start=timer()
-print(23*2.3)
-end=timer()
-print(start-end)

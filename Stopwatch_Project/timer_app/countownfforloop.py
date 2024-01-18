d72a22041607c4b01dbf01cc6f058f6115f14163
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-18 23:18:45 +0545

    while loop in countdown py

--- Stopwatch_Project/timer_app/countownfforloop.py
+++ Stopwatch_Project/timer_app/countownfforloop.py
@@ -1,5 +1,8 @@
- 
-# for i in reversed(range(1, 11)):
-#     print(i)
+#  using reversed concept
+for i in reversed(range(1, 11)):
+    print(i)
+
+
+    # using range
 for i in range(10,0,-1):
     print(i)
---
+++ Stopwatch_Project/timer_app/while loop for countdown.py
@@ -0,0 +1,5 @@
+i=10
+while i >  0:
+    print(i)
+    i -= 1
+# while loop for countdown functions
